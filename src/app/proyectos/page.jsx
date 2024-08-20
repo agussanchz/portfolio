9cd46fa2f0f164b61d35e751801f0c06c3198757
agustin <agustinsanchezx@gmail.com>
2024-08-20 19:43:26 -0300

    mejorando un poco los estilos en mobile

--- src/app/proyectos/page.jsx
+++ src/app/proyectos/page.jsx
@@ -4,69 +4,70 @@ import React from 'react'
 
 export default function Proyectos() {
     return (
-        <main className='py-20 flex flex-col justify-center items-center gap-28'>
+        <>
             <header className="flex justify-center items-center">
                 <Navbar />
             </header>
-            <div className='flex gap-8 w-[70rem] h-[21rem]'>
-                <Image
-                    src={'/CPS.png'}
-                    alt={'Centro privado salud'}
-                    width={550}
-                    height={1}
-                    className='rounded-2xl'
-                />
+            <main className='py-20 flex flex-col justify-center items-center px-10'>
+                <div className='flex flex-col items-center gap-6 p-20'>
+                    <h2 className='text-3xl font-bold text-[#727070]'>Centro Privado Salud</h2>
+                    <Image
+                        src={'/CPS.png'}
+                        alt={'Centro privado salud'}
+                        width={320}
+                        height={1}
+                        className='rounded-2xl'
+                    />
 
-                <div className='flex flex-col justify-center items-center gap-10 p-3'>
-                    <h2 className='text-5xl font-bold text-[#727070]'>Centro Privado Salud</h2>
-                    <p className='text-xl'>Pagina web para una clinica privada. Creada desde cero con React y Tailwind CSS. Actualmente en uso.</p>
-                    <div className='flex items-center text-center gap-6'>
-                        <span className='rounded-xl p-2 w-28 bg-[#F7F7F7] text-[#1399C4] '>React</span>
-                        <span className='rounded-xl  p-2 w-28 bg-[#003159] text-[#E5EAEE]'>Tailwind CSS</span>
-                        <span className='rounded-xl p-2 w-28 bg-[#1F2937]'>Deploy</span>
+                    <div className='flex flex-col justify-center items-center gap-6 '>
+                        <p className='text-sm w-svw p-3 text-center'>Pagina web para una clinica privada. Creada desde cero con React y Tailwind CSS. Actualmente en uso.</p>
+                        <div className='flex items-center text-center gap-1'>
+                            <span className='rounded-xl p-1 w-28 bg-[#F7F7F7] text-[#1399C4] '>React</span>
+                            <span className='rounded-xl p-1  w-28 bg-[#003159] text-[#E5EAEE]'>Tailwind CSS</span>
+                            <span className='rounded-xl p-1 w-28 bg-[#1F2937]'>Deploy</span>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className='flex gap-8 w-[70rem] h-[21rem]'>
-                <Image
-                    src={'/Barberia.png'}
-                    alt={'Barberia edgar'}
-                    width={550}
-                    height={1}
-                    className='rounded-2xl'
-                />
+                <div className='flex flex-col items-center gap-6 p-20'>
+                    <h2 className='text-3xl font-bold text-[#727070]'>Barberia Edgar</h2>
+                    <Image
+                        src={'/Barberia.png'}
+                        alt={'Barberia edgar'}
+                        width={320}
+                        height={1}
+                        className='rounded-2xl'
+                    />
 
-                <div className='flex flex-col justify-center items-center gap-10 p-3'>
-                    <h2 className='text-5xl font-bold text-[#727070]'>Barberia Edgar</h2>
-                    <p className='text-xl'>Turnero online para Barberia. Creada desde cero con React y Tailwind CSS. Actualmente en uso.</p>
-                    <div className='flex items-center text-center gap-6'>
-                        <span className='rounded-xl p-2 w-28 bg-[#F7F7F7] text-[#1399C4] '>React</span>
-                        <span className='rounded-xl  p-2 w-28 bg-[#003159] text-[#E5EAEE]'>Tailwind CSS</span>
-                        <span className='rounded-xl p-2 w-28 bg-[#1F2937]'>Deploy</span>
+                    <div className='flex flex-col justify-center items-center gap-10 p-3'>          
+                        <p className='text-sm w-svw p-3 text-center'>Turnero online para Barberia. Creada desde cero con React y Tailwind CSS. Actualmente en uso.</p>
+                        <div className='flex items-center text-center gap-1'>
+                            <span className='rounded-xl p-1 w-28 bg-[#F7F7F7] text-[#1399C4] '>React</span>
+                            <span className='rounded-xl  p-1 w-28 bg-[#003159] text-[#E5EAEE]'>Tailwind CSS</span>
+                            <span className='rounded-xl p-1 w-28 bg-[#1F2937]'>Deploy</span>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className='flex gap-8 w-[70rem] h-[21rem]'>
-                <Image
-                    src={'/ecommerce.png'}
-                    alt={'Ecommerce'}
-                    width={550}
-                    height={1}
-                    className='rounded-2xl'
-                />
-
-                <div className='flex flex-col justify-center items-center gap-10 p-3'>
-                    <h2 className='text-5xl font-bold text-[#727070]'>Ecommerce App</h2>
-                    <p className='text-xl'>Proyecto de ecommerce basico para practicar Next.js y tailwind css</p>
-                    <div className='flex items-center text-center gap-6'>
-                        <span className='rounded-xl p-2 w-28 bg-[#000000] text-[#FFFFFF] '>Next.js</span>
-                        <span className='rounded-xl  p-2 w-28 bg-[#003159] text-[#E5EAEE]'>Tailwind CSS</span>
-                        <span className='rounded-xl p-2 w-28 bg-[#1F2937]'>Deploy</span>
+                <div className='flex flex-col items-center gap-8 p-20'>
+                    <h2 className='text-3xl font-bold text-[#727070]'>Ecommerce App</h2>
+                    <Image
+                        src={'/ecommerce.png'}
+                        alt={'Ecommerce'}
+                        width={320}
+                        height={1}
+                        className='rounded-2xl'
+                    />
+                    <div className='flex flex-col justify-center items-center gap-10 p-3'>              
+                        <p className='text-sm w-svw p-3 text-center'>Proyecto de ecommerce basico para practicar Next.js y tailwind css</p>
+                        <div className='flex items-center text-center gap-1'>
+                            <span className='rounded-xl p-1 w-28 bg-[#000000] text-[#FFFFFF] '>Next.js</span>
+                            <span className='rounded-xl  p-1 w-28 bg-[#003159] text-[#E5EAEE]'>Tailwind CSS</span>
+                            <span className='rounded-xl p-1 w-28 bg-[#1F2937]'>Deploy</span>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </main>
+            </main>
+        </>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <nav className='shadow-2xl fixed shadow-[#524e4e] text-gray-400 bg-[#131414eb]  w-[30rem] p-3 rounded-full'>
+    <nav className='shadow-2xl fixed shadow-[#524e4e] text-gray-400 bg-[#131414eb]  w-[20rem] p-3 rounded-full top-3'>
         <ul className='flex justify-center gap-3 text-lg font-medium'>
             <Link href={'/'} className='hover:text-[#FFFFFF]'>Volver</Link>
         </ul>
